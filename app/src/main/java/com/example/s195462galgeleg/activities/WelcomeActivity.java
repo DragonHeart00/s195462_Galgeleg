d2b507dfbafe69aba455f3a750d2b76a1de611fc
kasassulaiman <s195462@student.dtu.dk>
2020-10-19 17:10:45 +0200

    i just added PreferenceManager to ScoreFragment

--- app/src/main/java/com/example/s195462galgeleg/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/s195462galgeleg/activities/WelcomeActivity.java
@@ -3,7 +3,9 @@ package com.example.s195462galgeleg.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private Button getStarted;
     private EditText name;
+    private SharedPreferences sharedPreferences;
 
 
     @Override
@@ -26,6 +29,8 @@ public class WelcomeActivity extends AppCompatActivity {
         getStarted = findViewById(R.id.start_button);
         name = findViewById(R.id.editTextTextPersonName);
 
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         getStarted.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +44,10 @@ public class WelcomeActivity extends AppCompatActivity {
                     Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
                     intent.putExtra("name",str);
                     startActivity(intent);
+
+                    // get from lecture 5
+                    String gemTekst = name.getText().toString();
+                    sharedPreferences.edit().putString("name", gemTekst).apply();
                 }
 
             }
--- app/src/main/java/com/example/s195462galgeleg/fragments/ScoreFragment.java
+++ app/src/main/java/com/example/s195462galgeleg/fragments/ScoreFragment.java
@@ -1,6 +1,8 @@
 package com.example.s195462galgeleg.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +15,12 @@ import com.example.s195462galgeleg.R;
 import com.example.s195462galgeleg.controller.PlayerAdapter;
 import com.example.s195462galgeleg.model.Player;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 public class ScoreFragment extends Fragment {
@@ -23,6 +29,7 @@ public class ScoreFragment extends Fragment {
     private RecyclerView recyclerView;
     private List<Player> playerList;
     private RecyclerView.Adapter adapter;
+    private SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,12 +42,23 @@ public class ScoreFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         playerList = new ArrayList<>();
 
+        // when we have a fragment
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-        Player player1 = new Player("Amer", "38400", "10.Oct.2020");
-        Player player2 = new Player("Mark", "34000", "12.May.2020");
+
+        // Add name and date to recyclerView
+        String gemtTekst = sharedPreferences.getString("name", "Ingen gemt editText fundet");
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time => " + c);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+
+
+        Player player1 = new Player(gemtTekst, formattedDate);
+
 
         playerList.add(player1);
-        playerList.add(player2);
+
 
 
 
