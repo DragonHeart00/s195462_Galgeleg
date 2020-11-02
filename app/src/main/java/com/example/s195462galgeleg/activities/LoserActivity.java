d4b9123a00a205fa5fac6f8540392b25e39ec3af
kasassulaiman <s195462@student.dtu.dk>
2020-11-02 14:46:36 +0100

    added emoji to activity_loser.xml and activity_winner.xml

--- app/src/main/java/com/example/s195462galgeleg/activities/LoserActivity.java
+++ app/src/main/java/com/example/s195462galgeleg/activities/LoserActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.s195462galgeleg.R;
